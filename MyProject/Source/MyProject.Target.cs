8c6822a0b2c8de9c5ca15a4a2c66a414e7b96c17
woshiabc55 <woshiabc55@users.noreply.github.com>
2026-05-29 12:28:54 +0000

    feat: Create Login Page
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

---
+++ MyProject/Source/MyProject.Target.cs
@@ -0,0 +1,13 @@
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class MyProjectTarget : TargetRules
+{
+	public MyProjectTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.AddRange(new string[] { "MyProject" });
+	}
+}
---
+++ MyProject/Source/MyProject/MyProject.Build.cs
@@ -0,0 +1,23 @@
+using UnrealBuildTool;
+
+public class MyProject : ModuleRules
+{
+	public MyProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+			"Slate",
+			"SlateCore",
+		});
+	}
+}
